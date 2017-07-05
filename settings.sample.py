8873bcc679966043638b8860b9aeadfee1bfa418
Eunchong Lee <lee11326@gmail.com>
2017-07-05 08:27:49 +0000

    add sleep and what_temp

--- settings.sample.py
+++ settings.sample.py
@@ -3,11 +3,10 @@ from __future__ import unicode_literals
 
 
 SLACK_TOKEN = None
-
 REDIS_URL = None
 
 # gevent pool size
 POOL_SIZE = 20
 
 # add your app name to this list
-APPS = ['hello_world', 'helper', 'giphy', 'ghost']
+APPS = ['hello_world', 'helper', 'giphy', 'ghost', 'rainy_day', 'what_temp', 'sleep']
