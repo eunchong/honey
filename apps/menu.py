bac2e5b09a53ecad34b02848278a20d6c911530c
iuasdofil <iuasdofil@gmail.com>
2017-07-11 04:38:45 +0000

    random idx edit

--- apps/menu.py
+++ apps/menu.py
@@ -10,7 +10,8 @@ def run(robot, channel, user, tokens):
     '''오늘의 점심 메뉴'''
     with open('./apps/menu.list', 'r') as file:
         menus = file.read().splitlines()
+    length = len(menus)
     R = random.Random()
-    idx = R.randrange(1,6)
+    idx = R.randrange(1, length)
     return channel, "오늘의 점심 메뉴!!! %s"%unicode(menus[idx], "utf-8")
 
