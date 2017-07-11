564178d4cbbc3666689cea33dd29d5fc03e027ee
iuasdofil <iuasdofil@gmail.com>
2017-07-11 04:31:16 +0000

    add_menu unicode edit

--- apps/add_menu.py
+++ apps/add_menu.py
@@ -4,11 +4,12 @@ from decorators import on_command
 
 import requests
 import random
+import codecs
 
 @on_command(['메뉴추가', 'add_menu'])
 def run(robot, channel, user, tokens):
     '''점심 메뉴 추가!!!!'''
-    with open('./apps/menu.list', 'a') as file:
-        file.write("%s\n"%unicode(tokens[0], "utf-8"))
+    with codecs.open('./apps/menu.list', 'a', encoding="utf-8") as file:
+        file.write("%s\n"%tokens[0])
     return channel, "%s 메뉴 추가 완료!!"%tokens[0]
 
